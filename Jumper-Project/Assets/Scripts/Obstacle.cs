6b78fda22bb4d9a8594edc098ec29e972a6b372d
HalimaR <rahimi.halima17@gmail.com>
2020-11-29 23:35:06 +0100

    jummper

---
+++ Jumper-Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float speed;
+    //private Character character;
+
+    private bool hit = false;
+
+    private void Update()
+    {
+        
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        speed = Random.Range(1f, 15f);
+        transform.position += speed * transform.forward * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "end")
+        {
+            hit = true;
+            Destroy(gameObject);
+           //character = character.HitCollider(hit);
+            
+
+        }
+        
+    }
+}
